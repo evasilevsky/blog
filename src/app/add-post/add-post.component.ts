898f49e63780957cc68e2ed64c5d375816552299
Eric Vasilevsky <evasilevsky@gmail.com>
2017-01-30 22:49:51 -0800

    meh

--- src/app/add-post/add-post.component.ts
+++ src/app/add-post/add-post.component.ts
@@ -11,31 +11,27 @@ import { PostService } from 'app/post.service';
 export class AddPostComponent implements OnInit {
   private title: string;
   private body: string;
+
+  private canAdd: boolean = false;
   constructor(
     private af: AngularFire,
     private postService: PostService
   ) { }
 
   ngOnInit() {
+    this.af.auth.subscribe(auth => {
+      this.af.database.object("/users/" + auth.uid + "/role")
+        .subscribe(t => {
+          if (t.$value === "admin") {
+            this.canAdd = true;
+          } else {
+            this.canAdd = false;
+          }
+      });
+    });
   }
 
   private post = () => {
     this.postService.addPost(this.title, this.body)
-    // if ("localStorage" in window) {
-    //   let storedPosts = localStorage.getItem("posts");
-    //   let posts: Post[] = [];
-    //   let maxValue: number = 0;
-    //   if (storedPosts) {
-    //     posts = JSON.parse(storedPosts);
-    //     maxValue = Math.max.apply(Math,posts.map(post => {
-    //       return post.id;
-    //     }));
-    //   }
-
-    //   posts.push(new Post(maxValue + 1, this.title, this.body));
-    //   localStorage.setItem("posts", JSON.stringify(posts));
-    //   this.title = "";
-    //   this.body = "";
-    // }
   }
 }
