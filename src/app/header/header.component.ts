564f71e1fcd266748360fe2b390039b4df15af63
Eric Vasilevsky <evasilevsky@gmail.com>
2017-01-31 17:31:57 -0800

    minor styles

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   private title: string = "Fantasical Blog";
+  private author: string = "Eric Vasilevsky";
   constructor(
   ) { }
 
